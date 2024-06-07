8d6c36ea7dee572028452340e62e0943b80ece06
gebby123 <tefayegebrehiwot123@gmail.com>
2024-06-07 13:34:00 +0300

    it is build problem solved

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 5000, // Adjust the limit as needed
+  },
 });
